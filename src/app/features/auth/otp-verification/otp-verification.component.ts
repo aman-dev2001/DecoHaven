b8dd50f782c48cd3a481d5805c5e5679884e24dd
Aman <amankushwaha667@gamil.com>
2025-11-11 17:08:10 +0530

    add auth pages layout

--- src/app/features/auth/otp-verification/otp-verification.component.ts
+++ src/app/features/auth/otp-verification/otp-verification.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-otp-verification',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './otp-verification.component.html',
-  styleUrl: './otp-verification.component.css'
+  styleUrls: ['./otp-verification.component.css'],
 })
 export class OtpVerificationComponent {
+  otpValues: string[] = ['', '', '', ''];
 
+  moveFocus(event: any, index: number) {
+    const input = event.target;
+    if (input.value.length === 1 && index < this.otpValues.length - 1) {
+      const nextInput = input.parentElement.nextElementSibling?.querySelector('input');
+      nextInput?.focus();
+    }
+  }
+
+  onSubmit() {
+    const otp = this.otpValues.join('');
+    console.log('Entered OTP:', otp);
+    alert(`Your OTP is ${otp}`);
+  }
+
+  resendOtp() {
+    alert('OTP has been resent successfully!');
+  }
 }
